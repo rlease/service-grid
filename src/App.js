0e1fd19a55aa5f1e479e46f697ff0171e0861220
Ryan Lease <leas5040@gmail.com>
2018-10-02 21:50:22 -0700

    Get a basic svg hex displaying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
+    const Point = (x, y) => {
+      return { x, y };
+    };
+
+    const Hexagon = ({ size, sides = 6 }) => {
+      // TODO: Add offsets to this so we can create more than one hex
+      const root2 = Math.sqrt(2);
+      let points = [
+        Point(0, (size * root2) / 2),
+        Point(size / root2, size * root2),
+        Point(size + size / root2, size * root2),
+        Point(size + (size * 2) / root2, (size * root2) / 2),
+        Point(size + size / root2, 0),
+        Point(size / root2, 0)
+      ];
+
+      return (
+        <polygon
+          points={points.map(point => `${point.x},${point.y}`).join(" ")}
+        />
+      );
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <svg viewBox="0 0 100 100">
+        <Hexagon size={5} />
+      </svg>
     );
   }
 }
